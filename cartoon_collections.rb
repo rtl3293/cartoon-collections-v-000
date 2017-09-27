030c8d7ae55080033312f54c0976a566f95cbbcd
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 18:07:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarf, i|
     puts "#{i} #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
