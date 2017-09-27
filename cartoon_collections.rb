0067003af98bba2daf61998c9f5c30e7f4bfbea6
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 18:15:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
+
 end
