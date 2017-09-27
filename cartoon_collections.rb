6a37957e40ed2722f0c72af5d8b48de9eac9555a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 18:18:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if array.include?(cheese)
-      return true
+      return cheese
     end
   end
 end
